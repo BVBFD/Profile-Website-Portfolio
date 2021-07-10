d48ff3007effad2127a94ef14be13f48bd4a7e08
BVBFD <lsevina126@gmail.com>
2021-07-10 18:58:04 +0900

    JS : Modify the toggle button of the navbar

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const navbar = document.querySelector('.nav-bar');
 const origin_logo = document.querySelector('.origin-logo');
 const changed_logo = document.querySelector('.changed-logo');
+const barsToggleBtn = document.querySelector('.bars-toggle-btn');
 
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
@@ -9,8 +10,10 @@ document.addEventListener('scroll', () => {
         navbar.classList.add('nav-bar-color-bgcolor');
         origin_logo.classList.remove('logo-show-up');
         changed_logo.classList.add('logo-show-up');
+        barsToggleBtn.classList.add('bars-toggle-btn-change-bgColor');   
     }else{
         navbar.classList.remove('nav-bar-color-bgcolor');
         changed_logo.classList.remove('logo-show-up');
+        barsToggleBtn.classList.remove('bars-toggle-btn-change-bgColor'); 
     }
 });
